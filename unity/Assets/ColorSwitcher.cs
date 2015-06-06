7246945f77566eb5f37dcdc6b29e8b4131b2971d
doelia <kyxalia@gmail.com>
2015-06-06 20:45:17 +0200

    Avancement

--- unity/Assets/ColorSwitcher.cs
+++ unity/Assets/ColorSwitcher.cs
@@ -7,11 +7,15 @@ public class ColorSwitcher : Switcher {
 	
 	override public void changeIt(int id) {
 		Color sp = colors[id-1];
-		this.GetComponent<SpriteRenderer>().color = sp;
+		setColor (sp);
 	}
 	
 	override public int getSize() {
 		return colors.Length;
+	}
+
+	public void setColor(Color c) {
+		this.GetComponent<SpriteRenderer>().color = c;
 	}
 
 }
